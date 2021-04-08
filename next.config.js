6cc743adf8c6dbe129562c0dacf01f5d899d0c35
johnbxu <johnbxu@gmail.com>
2021-04-07 20:54:26 -0400

    Remove nextauth

--- next.config.js
+++ next.config.js
@@ -1,5 +1,2 @@
 module.exports = {
-  env: {
-    API_URL: process.env.API_URL
-  }
 }
--- pages/index.js
+++ pages/index.js
@@ -3,77 +3,20 @@ import { getSession, signIn, signOut } from 'next-auth/client';
 import Link from 'next/link';
 
 export default function Home({ posts, session }) {
-  const signInButtonNode = () => {
-    if (session) {
-      return false;
-    }
-
-    return (
-      <div>
-        <Link href="/api/auth/signin">
-          <button
-            onClick={(e) => {
-              e.preventDefault();
-              signIn();
-            }}
-          >
-            Sign In
-          </button>
-        </Link>
-      </div>
-    );
-  };
-
-  const signOutButtonNode = () => {
-    if (!session) {
-      return false;
-    }
-
-    return (
-      <div>
-        <Link href="/api/auth/signout">
-          <button
-            onClick={(e) => {
-              e.preventDefault();
-              signOut();
-            }}
-          >
-            Sign Out
-          </button>
-        </Link>
-      </div>
-    );
-  };
-
-  if (!session) {
-
-    return (
-      <div>
-        {signOutButtonNode()}
-        {signInButtonNode()}
-        <h1>You are not authorized</h1>
-      </div>
-    );
-  }
-
   return (
     <div>
-      {signOutButtonNode()}
-      {signInButtonNode()}
       <PostList posts={posts} />
     </div>
   );
 }
 
-export const getServerSideProps = async ({ req }) => {
-  const session = await getSession({ req });
+export const getServerSideProps = async () => {
   const res = await fetch(`${process.env.API_URL}/posts`);
   const posts = await res.json();
 
   return {
     props: {
       posts,
-      session,
     },
   };
 };
